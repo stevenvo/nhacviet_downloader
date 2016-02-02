9019b9f4f8dbe758d1fe9d87fee8b9b3f046da3c
Steven Vo <findme@stevenvo.info>
2016-02-02 13:04:27 -0800

    remove pyc

--- nhacviet_downloader/spiders/nhacso.py
+++ nhacviet_downloader/spiders/nhacso.py
@@ -3,14 +3,23 @@ import scrapy
 
 
 class NhacsoSpider(scrapy.Spider):
+    # ARGS
+    album_url = ""
+    
     name = "nhacso"
     allowed_domains = ["nhacso.net"]
-    start_urls = (
-        'http://www.nhacso.net/',
-    )
+    # start_urls = (
+    #     'http://www.nhacso.net/',
+    # )
+    
+    def __init__(self, *args, **kwargs):
+      super(NhacsoSpider, self).__init__(*args, **kwargs)
+      self.album_url = kwargs.get('url')
+      self.start_urls = [self.album_url]
 
     def parse(self, response):
-        pass
+        inspect_response(response, self)
+
 
 
 # response.xpath('//div[@class="song"]/h1/a/text()').extract()
